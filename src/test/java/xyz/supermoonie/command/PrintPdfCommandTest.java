3019eac4e04d91c204242f1dcaec9cbfdccfbe87
moonie <wangchao_moonie@163.com>
2018-05-03 07:29:10 +0800

    printPdf test

--- src/test/java/xyz/supermoonie/command/PrintPdfCommandTest.java
+++ src/test/java/xyz/supermoonie/command/PrintPdfCommandTest.java
@@ -1,9 +1,9 @@
 package xyz.supermoonie.command;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import org.junit.Test;
 import xyz.supermoonie.controller.WebViewController;
+import xyz.supermoonie.expection.ExpectedConditions;
+import xyz.supermoonie.wait.Wait;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -20,16 +20,18 @@ public class PrintPdfCommandTest {
         try {
             controller = new WebViewController(new InetSocketAddress("127.0.0.1", 7100));
             LoadCommand loadCommand = new LoadCommand("https://juejin.im/entry/5ae2c177f265da0b722ad90b");
-            System.out.println(loadCommand.generate());
             String loadData = controller.sendCommand(loadCommand);
             System.out.println(loadData);
+
+            Wait wait = new Wait(controller, 20000, 300);
+            wait.until(ExpectedConditions.loadFinished());
 
             ExecCommand loadImgCommand = new ExecCommand("" +
                     "var imgArr = document.querySelector('article').getElementsByTagName('img');" +
                     "for(var i = 0; i < imgArr.length; i ++) {" +
                     "var src = imgArr[i].dataset.src;" +
-                    "var height = imgArr[i].dataset.hight;" +
-                    "var width = imgArr[i].dataset.widht;" +
+                    "var height = imgArr[i].dataset.height;" +
+                    "var width = imgArr[i].dataset.width;" +
                     "imgArr[i].src = src;" +
                     "imgArr[i].width = width;" +
                     "imgArr[i].height = height;" +
@@ -37,7 +39,8 @@ public class PrintPdfCommandTest {
             String loadImgData = controller.sendCommand(loadImgCommand);
             System.out.println(loadImgData);
 
-            Thread.sleep(5000);
+            wait.until(ExpectedConditions.loadFinished());
+
             PrintPdfCommand printPdfCommand = new PrintPdfCommand();
             String printPdfData = controller.sendCommand(printPdfCommand);
             System.out.println(printPdfData);
